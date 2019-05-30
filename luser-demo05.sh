e06b366c58f353b92bc00a8a540fac80fb04ba40
Andi <familie.mayer@aon.at>
2019-05-30 12:22:05 +0200

    generate passwrd

--- luser-demo05.sh
+++ luser-demo05.sh
@@ -2,13 +2,22 @@
 
 # random passwords
 PASSWORD="${RANDOM}"
-echo "${PASSWORD}"
+echo $RANDOM
 
 PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
 echo "${PASSWORD}"
 
-PASSWORD=$(date +%s%n)
+PASSWORD="$(date +%s)"
 echo "${PASSWORD}"
 
-PASSWORD=$(date +%s | sha256sum | head -c32)
+PASSWORD=$(date +%s%N)
 echo "${PASSWORD}"
+
+PASSWORD=$(date %s%N | sha256sum | head -c8)
+echo "${PASSWORD}"
+
+PASSWORD=$(date + %s{RANDOM} | sha256sum | head -c16)
+echo "${PASSWORD}"
+
+SPECIAL_CHAR=$(echo '!@#$%^&*()_-+=' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHAR}"
